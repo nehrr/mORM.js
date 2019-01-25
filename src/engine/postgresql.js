4b1a11e233e3f43f983f6bb6514d5b12219347b3
ERNOUL <iamkapitaan@gmail.com>
2019-01-25 10:41:29 +0100

    connection to pg working

--- src/engine/postgresql.js
+++ src/engine/postgresql.js
@@ -1,4 +1,27 @@
+import Core from "./core";
+import { Client } from "pg";
+
 export default class PostgreSQL extends Core {
-  // ...
-  async initialize() {}
+  constructor(options) {
+    super(options);
+  }
+
+  async initialize() {
+    const { host, port, username, password, database } = this;
+
+    this.client = new Client({
+      user: username,
+      host,
+      database,
+      password,
+      port
+    });
+
+    try {
+      await this.client.connect();
+      console.log("connected");
+    } catch (e) {
+      throw new Error(`Database ${database} does not exist`);
+    }
+  }
 }
